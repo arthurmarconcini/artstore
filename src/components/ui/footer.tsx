40f1c370f2e2a513cc4467ca241e5058a981f602
Arthur <arthur.m.ramos@hotmail.com>
2023-10-26 21:05:01 -0300

    feat: Create Footer Component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-accent px-7 py-5 ">
+      <p className="text-xs opacity-75">
+        © 2023 Copyright <span className="font-semibold">FSW Store</span>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
