33bc67b6c62b21d4eb75a9a377513d0bc7303a5c
Arthur <arthur.m.ramos@hotmail.com>
2023-10-28 10:25:13 -0300

    feat: Create Icons constant and update dependents components

--- src/app/(home)/components/CategoryItem.tsx
+++ src/app/(home)/components/CategoryItem.tsx
@@ -1,34 +1,18 @@
 import { Badge } from "@/components/ui/badge";
+import { CATEGORIES_ICONS } from "@/constants/CategoryIcons";
 import { Category } from "@prisma/client";
-import {
-  HeadphonesIcon,
-  KeyboardIcon,
-  MonitorIcon,
-  MouseIcon,
-  PlaneIcon,
-  SpeakerIcon,
-} from "lucide-react";
 
 interface CategoryItemProps {
   category: Category;
 }
 
 function CategoryItem({ category }: CategoryItemProps) {
-  const icons = {
-    keyboards: <KeyboardIcon size={16} />,
-    mouses: <MouseIcon size={16} />,
-    headphones: <HeadphonesIcon size={16} />,
-    monitors: <MonitorIcon size={16} />,
-    speakers: <SpeakerIcon size={16} />,
-    mousepads: <PlaneIcon size={16} />,
-  };
-
   return (
     <Badge
       variant={"outline"}
       className="flex w-full justify-center gap-2 py-3 font-bold"
     >
-      {icons[category.slug as keyof typeof icons]}
+      {CATEGORIES_ICONS[category.slug as keyof typeof CATEGORIES_ICONS]}
       <span className="text-xs">{category.name}</span>
     </Badge>
   );
